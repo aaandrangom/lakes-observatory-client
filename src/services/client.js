948114836b08486a30e66a476111127e39f829b0
Anthony Andrango <sir-ankun>
2024-09-21 19:44:49 -0500

    added api url

--- src/services/client.js
+++ src/services/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_API_URL;
+const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_API_URL_PRODUCTION;
 
 const apiClient = axios.create({
     baseURL: BASE_URL,
